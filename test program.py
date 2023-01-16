ecae2e05af7b615826bdaf9627bf85b14016aa86
Arslan1004 <122795524+Arslan1004@users.noreply.github.com>
2023-01-16 05:57:45 -0800

    Add files via upload

---
+++ test program.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#print("hello world")
+#print("Arslan nawaz")
+#my_name="Arslan"
+#print(my_name)
+#x=2*3
+#print(x)
+#first_number=2
+#second_number=4
+#total=first_number+second_number
+#print(total)
+#my_name="Affan"
+#print(my_name)
+my_expression=40/4+2
+my_ans=my_expression
+#print(my_ans)
+#x=2
+#x+=10
+#print(x)
+#my_name="arslan"
+#my_name="nawaz"
+#print(my_name)
+#x=10%3
+#print(x*5)
+#user_name=input("enter username")
+#print("my name is" +user_name)
+#
+
+#test_text=input("enter your text")
+#test_write=str(test_text)
+#print("you enter text is",test_write)
+#is_rain = True
+#if is_rain:
+    #print("i will go outside")
+    
+    
+    
+  #  is_rain = False
+    #if is_rain:
+        #print("i will outside")
+#else:
+ #   print("i will not go outside")
+ 
+ 
+ 
+ 
+ 
+#radius=input("radius of circle is=")
+#radius_value=int(radius)
+#area=3.14*(radius_value**2)
+#print("Areaofcircle=",area)
+
+
+
+#radius=input("raduis of circle")
+#radius_value=float(radius)
+#area_circle=3.14*(radius_value**2)
+#print("areaofcircleis=",area_circle)
+
+
+radius=input("raduis of circle")
+radius_value=float(radius)
+area_circle=3.14*pow(radius_value,2)
+print("areaofcircleis=",area_circle)
