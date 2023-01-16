2cfa56238a7484ce310eda5b9c78b67de7974b5b
Arslan1004 <122795524+Arslan1004@users.noreply.github.com>
2023-01-16 06:16:12 -0800

    radius of circle program

---
+++ calculate radius of circle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 16 06:07:12 2023
+
+@author: abcd
+"""
+
+radius=input("Enter radius of circle = ")
+radius_value=float(radius)
+area_of_circle=3.14*pow(radius_value,2)
+print("Area of circle is =",area_of_circle)
